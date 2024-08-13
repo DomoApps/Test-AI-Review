d499bcab39aee4a109a17bb6d81f98d28b423ada
Jonathan Tiritilli <jon.tiritilli@domo.com>
2024-08-13 14:32:53 -0700

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NTUzMg/spectral.js?branch_slug=main&token=729ba681-d8db-41ca-8c1c-fcb81af71893";
+export default { extends: ruleset };
